d510db4c6d84ce668808b7b0852069bfc210c9e3
Esnaira <esnairasulaiman@gmail.com>
2022-06-03 15:12:45 +0800

    First commit

---
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/Authors/GetAuthorListDto.cs
@@ -0,0 +1,9 @@
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Authors
+{
+    public class GetAuthorListDto : PagedAndSortedResultRequestDto
+    {
+        public string Filter { get; set; }
+    }
+}
---
+++ aspnet-core/src/Acme.BookStore.Domain.Shared/Authors/AuthorConsts.cs
@@ -0,0 +1,7 @@
+namespace Acme.BookStore.Authors
+{
+    public static class AuthorConsts
+    {
+        public const int MaxNameLength = 64;
+    }
+}
